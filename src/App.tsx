8bbee60ccddd2595e21fb477cf83244098e886d0
Betül <betulaltunyuva@gmail.com>
2026-02-24 20:16:36 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarımı ve Programlama</h1>
+      <h2>LAB - 1</h2>
+      <p>Ad Soyad: Betül Altunyuva</p>
+      <p>Öğrenci No: XXXXX</p>
+    </div>
+  )
+}
+
+export default App
